166bbda3829a15176764a74de7646eb966d67ae3
Manoj Reddy <manojreddypalla@gmail.com>
2025-10-17 21:23:38 +0530

    tr

---
+++ FrontEnd/test.py
@@ -0,0 +1,26 @@
+import streamlit as st 
+
+import os 
+
+"hello "
+st.write("hello world 123 jvbndskljvkdsjbnvdjbnvikloadjvb ")
+
+
+9+4+5+8+2+6+4+5+56+5+5+5+5+5+5+5+5+5+5+47+4+4+4+4+4+4+4+4+44+4+4+4+4+4+4+47+4+47+45+47+1
+
+presses = st.button("press me asshole ")
+
+if presses == True:
+    "yes ur "
+
+
+
+code = """ 
+def greet('hello',name):
+    print("bnvkljbfsvkljbn")"""
+
+
+st.code(code,language="python")
+
+st.divider()
+st.image(os.path.join(os.getcwd(), "thunderstorm-countryside.jpg"))
